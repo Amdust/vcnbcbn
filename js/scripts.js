7fd55e270a8b293301ae6abd7dbe3d8e9bc1cb39
Amdust <104697429+Amdust@users.noreply.github.com>
2022-08-08 22:58:35 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+new Swiper('.slider__container', {
+  slidesPerView: 2.7,
+  spaceBetween: 32,
+  loop: true,
+  
+  autoplay:{
+    delay: 1500,
+    stopOnLastSlide:false,
+    disableOnIteraction:false,
+  },
+  speed:1500,
+  // breakpoints:{
+
+  // }
+});
+
+new Swiper('.slider__container-2', {
+  slidesPerView: 2.7,
+  spaceBetween: 32,
+  loop: true,
+  autoplay:{
+    delay: 1500,
+    stopOnLastSlide:false,
+    disableOnIteraction:false,
+  },
+  speed:1500,
+  // breakpoints:{
+
+  // }
+});
